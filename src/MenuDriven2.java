d1ac06b2cde2efb13532385375c02d9b4f0bdbca
vishnu1646 <vishnulance98@gmail.com>
2022-11-21 12:49:14 +0530

    dev

--- src/MenuDriven2.java
+++ src/MenuDriven2.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class MenuDriven2 {
     public static void main(String[] args) {
+int num1,num2,num3;
+int choice;
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("1 for Largest of 3");
+            System.out.println("2 for Smallest of 3");
+            System.out.println("Prime or not ");
+            System.out.println( "Even or not");
+            System.out.println("Exit");
+            choice =sc.nextInt();
 
+
+        }
     }
 }
